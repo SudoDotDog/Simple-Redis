2aa2afc1294cd26e214cdcd2494cf395c89ddf7a
WMXPY <wm@mengw.io>
2019-06-30 18:33:10 -0500

    style fix

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,16 @@ const port: number = 8080;
 
 const redis: Redis.RedisClient = Redis.createClient(6379, 'localhost');
 
-app.get('/set', (req: Express.Request, res: Express.Response) => {
+app.get('/set', (_: Express.Request, res: Express.Response) => {
     redis.set('color', 'red');
     res.send('Hello World!');
 });
 
-app.get('/get', async (req: Express.Request, res: Express.Response) => {
+app.get('/get', async (_: Express.Request, res: Express.Response) => {
 
     redis.get('color', (err, color) => {
         res.send(color);
-    })
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
